534ffe21863e523d985087ee1009b177d78a8c7f
Aaron Heckmann <aaron.heckmann+github@gmail.com>
2021-11-06 12:09:32 +0000

    lint

--- src/arweave-cost-calc.jsx
+++ src/arweave-cost-calc.jsx
@@ -1,8 +1,8 @@
-import React from 'react'
-import { calculate } from '@metaplex/arweave-cost';
-import { useState } from 'react';
+import React from "react";
+import { calculate } from "@metaplex/arweave-cost";
+import { useState } from "react";
 
-function ArweaveCostEstimator () {
+function ArweaveCostEstimator() {
   const [cost, setCost] = useState(0);
 
   const handleChange = async (e) => {
@@ -12,25 +12,37 @@ function ArweaveCostEstimator () {
 
     const bytes = kb * 1024;
     const result = await calculate([bytes]);
-    console.log(result)
+    console.log(result);
 
-    const sol = Math.round((result.solana + Number.EPSILON) * 100000000) /100000000;
+    const sol =
+      Math.round((result.solana + Number.EPSILON) * 100000000) / 100000000;
     setCost(sol);
   };
 
   return (
-    <form style={{border: '1px solid #444', padding: '8px'}} action='#'>
+    <form style={{ border: "1px solid #444", padding: "8px" }} action="#">
       <div>
-        <label for='fileSize' style={{marginRight: '10px'}}>Total storage size of asset and metadata.json (in kilobytes):</label>
-        <input id='fileSize' autoComplete='off' type='number' onChange={handleChange}/>
+        <label for="fileSize" style={{ marginRight: "10px" }}>
+          Total storage size of asset and metadata.json (in kilobytes):
+        </label>
+        <input
+          id="fileSize"
+          autoComplete="off"
+          type="number"
+          onChange={handleChange}
+        />
       </div>
       <div>
-        <span style={{display: 'inline-block', marginRight: '10px'}}>Estimated Arweave storage cost:</span>
+        <span style={{ display: "inline-block", marginRight: "10px" }}>
+          Estimated Arweave storage cost:
+        </span>
         <span>{cost}</span>&nbsp;SOL
       </div>
-      <p style={{color:'#888', fontSize:'0.8em', margin:'10px 0 0'}}><em >Fees are dynamic. Pricing is determined by the Arweave network</em></p>
+      <p style={{ color: "#888", fontSize: "0.8em", margin: "10px 0 0" }}>
+        <em>Fees are dynamic. Pricing is determined by the Arweave network</em>
+      </p>
     </form>
-  )
-};
+  );
+}
 
 export default ArweaveCostEstimator;
