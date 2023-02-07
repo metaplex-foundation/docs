efb8837d0a41f83ce2a5736015e124f7fb114c71
Loris Leiva <loris.leiva@gmail.com>
2023-02-07 13:34:53 +0000

    Add PNFTs in the Token Standard page

--- docs/01-programs/01-token-metadata/04-token-standard.md
+++ docs/01-programs/01-token-metadata/04-token-standard.md
@@ -182,3 +182,57 @@ These are the "standard" non-fungible tokens the community is already familiar w
 
 </AccordionCode>
 </Accordion>
+
+## The Programmable Non-Fungible Standard
+
+This standard is similar to the **Non-Fungible** standard above, except that the underlying token account is kept frozen at all times to ensure nobody can transfer, lock or burn Programmable NFTs without going through the Token Metadata program. This enables creators to define custom authorization rules for their NFTs such as enforcing secondary sales royalties.
+
+You can [read more about Programmable NFTs here](https://github.com/metaplex-foundation/metaplex-program-library/blob/master/token-metadata/program/ProgrammableNFTGuide.md).
+
+<Accordion>
+<AccordionTokenStandard
+    fields={tokenStandard.programmableNonFungible}
+    open={true}
+></AccordionTokenStandard>
+<AccordionCode title="Example">
+
+```json
+{
+  "name": "SolanaArtProject #1",
+  "description": "Generative art on Solana.",
+  "image": "https://arweave.net/26YdhY_eAzv26YdhY1uu9uiA3nmDZYwP8MwZAultcE?ext=jpeg",
+  "animation_url": "https://arweave.net/ZAultcE_eAzv26YdhY1uu9uiA3nmDZYwP8MwuiA3nm?ext=glb",
+  "external_url": "https://example.com",
+  "attributes": [
+    {
+      "trait_type": "trait1",
+      "value": "value1"
+    },
+    {
+      "trait_type": "trait2",
+      "value": "value2"
+    }
+  ],
+  "properties": {
+    "files": [
+      {
+        "uri": "https://www.arweave.net/abcd5678?ext=png",
+        "type": "image/png"
+      },
+      {
+        "uri": "https://watch.videodelivery.net/9876jkl",
+        "type": "unknown",
+        "cdn": true
+      },
+      {
+        "uri": "https://www.arweave.net/efgh1234?ext=mp4",
+        "type": "video/mp4"
+      }
+    ],
+    "category": "video",
+  }
+}
+```
+
+</AccordionCode>
+</Accordion>
--- src/token-metadata/token-standard.js
+++ src/token-metadata/token-standard.js
@@ -1,3 +1,55 @@
+const nonFungibleStandard = [
+  {
+    name: "name",
+    type: "string",
+    description: "Name of the asset.",
+  },
+  {
+    name: "symbol",
+    type: "string",
+    description: "Symbol of the asset.",
+  },
+  {
+    name: "description",
+    type: "string",
+    description: "Description of the asset.",
+  },
+  {
+    name: "image",
+    type: "string",
+    description: "URI pointing to the asset's logo.",
+  },
+  {
+    name: "animation_url",
+    type: "string",
+    description: "URI pointing to the asset's animation.",
+  },
+  {
+    name: "external_url",
+    type: "string",
+    description:
+      "URI pointing to an external URL defining the asset — e.g. the game's main site.",
+  },
+  {
+    name: "attributes",
+    type: "array",
+    description:
+      "Array of attributes defining the characteristics of the asset.",
+    fields: [
+      {
+        name: "trait_type",
+        type: "string",
+        description: "The type of attribute.",
+      },
+      {
+        name: "value",
+        type: "string",
+        description: "The value for that attribute.",
+      },
+    ],
+  },
+];
+
 export default {
   fungible: [
     {
@@ -72,55 +124,6 @@ export default {
       ],
     },
   ],
-  nonFungible: [
-    {
-      name: "name",
-      type: "string",
-      description: "Name of the asset.",
-    },
-    {
-      name: "symbol",
-      type: "string",
-      description: "Symbol of the asset.",
-    },
-    {
-      name: "description",
-      type: "string",
-      description: "Description of the asset.",
-    },
-    {
-      name: "image",
-      type: "string",
-      description: "URI pointing to the asset's logo.",
-    },
-    {
-      name: "animation_url",
-      type: "string",
-      description: "URI pointing to the asset's animation.",
-    },
-    {
-      name: "external_url",
-      type: "string",
-      description:
-        "URI pointing to an external URL defining the asset — e.g. the game's main site.",
-    },
-    {
-      name: "attributes",
-      type: "array",
-      description:
-        "Array of attributes defining the characteristics of the asset.",
-      fields: [
-        {
-          name: "trait_type",
-          type: "string",
-          description: "The type of attribute.",
-        },
-        {
-          name: "value",
-          type: "string",
-          description: "The value for that attribute.",
-        },
-      ],
-    },
-  ],
+  nonFungible: nonFungibleStandard,
+  programmableNonFungible: nonFungibleStandard,
 };
